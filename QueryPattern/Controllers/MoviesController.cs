79d5711d5bf4f9fc4ec80dfa2e7beeb04fa4954b
Ronnie Overby <roverby@coretechs.net>
2014-06-30 18:24:09 -0400

    sorting

---
+++ QueryPattern/Controllers/MoviesController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using QueryPattern.Models;
+
+namespace QueryPattern.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private const string QuerySessionKey = "MoviesController.Query";
+
+        public MoviesQuery Query
+        {
+            get
+            {
+                var moviesQuery = Session[QuerySessionKey] as MoviesQuery;
+                return moviesQuery ?? (Query = new MoviesQuery());
+            }
+            set { Session[QuerySessionKey] = value; }
+        }
+
+        public ActionResult Index()
+        {
+            var movies = ExecuteMoviesQuery(Query);
+            return View(movies);
+        }
+
+        public ActionResult Sort(string property)
+        {
+            if (Query.Sort == property)
+            {
+                // toggle direction
+                Query.SortDescending = !Query.SortDescending;
+            }
+            else
+            {
+                // change sort
+                Query.Sort = property;
+                Query.SortDescending = false;
+            }
+
+
+            var movies = ExecuteMoviesQuery(Query);
+            return View("Index", movies);
+        }
+
+        private static Movie[] ExecuteMoviesQuery(MoviesQuery moviesQuery)
+        {
+            var query = FakeDatabase.Movies.AsQueryable();
+
+            // filters
+            if (!string.IsNullOrWhiteSpace(moviesQuery.Title))
+                query = query.Where(x => x.Title.Equals(moviesQuery.Title, StringComparison.OrdinalIgnoreCase));
+
+            if (moviesQuery.Year != null)
+                query = query.Where(x => x.Year == moviesQuery.Year);
+
+            // sorting
+            moviesQuery.Sort = moviesQuery.Sort ?? "";
+            switch (moviesQuery.Sort.ToLowerInvariant())
+            {
+                case "title":
+                    query = moviesQuery.SortDescending
+                        ? query.OrderByDescending(x => x.Title)
+                        : query.OrderBy(x => x.Title);
+                    break;
+
+                case "year":
+                    query = moviesQuery.SortDescending
+                        ? query.OrderByDescending(x => x.Year)
+                        : query.OrderBy(x => x.Year);
+                    break;
+
+                default:
+                    query = moviesQuery.SortDescending
+                        ? query.OrderByDescending(x => x.Id)
+                        : query.OrderBy(x => x.Id);
+                    break;
+            }
+
+            // paging
+            if (moviesQuery.Skip.HasValue)
+                query = query.Skip(moviesQuery.Skip.Value);
+
+            if (moviesQuery.Take.HasValue)
+                query = query.Take(moviesQuery.Take.Value);
+
+            return query.ToArray();
+        }
+    }
+
+    public class MoviesQuery
+    {
+        // filters
+        public string Title { get; set; }
+        public int? Year { get; set; }
+
+        // sorting
+        public string Sort { get; set; }
+        public bool SortDescending { get; set; }
+
+        // paging
+        public int? Skip { get; set; }
+        public int? Take { get; set; }
+
+        public MoviesQuery()
+        {
+            Sort = "Id";
+        }
+
+    }
+}
---
+++ QueryPattern/Models/FakeDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace QueryPattern.Models
+{
+    public static class FakeDatabase
+    {
+        private static int _seq = 0;
+
+        public static readonly List<Movie> Movies = new List<Movie>
+        {
+            new Movie
+            {
+                Id = NextId(),
+                Title = "Star Wars",
+                Year = 1977
+            },
+            new Movie
+            {
+                Id = NextId(),
+                Title = "Lord of the Rings",
+                Year = 2001
+            },
+            new Movie
+            {
+                Id = NextId()
+                ,
+                Title = "The Fountain",
+                Year = 2006
+            },
+            new Movie
+            {
+                Id = NextId(),
+                Title = "Every Which Way But Loose",
+                Year = 1978
+            },
+
+        };
+
+        static int NextId()
+        {
+            return Interlocked.Increment(ref _seq);
+        }
+
+
+    }
+}
